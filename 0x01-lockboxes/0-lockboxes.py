7fc0acbf762740097079ed9e629ef8520b0c25c2
samsonalxgit <samsonmanie2022@gmail.com>
2023-08-06 12:53:47 -0700

    interview

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,19 @@
+def canUnlockAll(boxes):
+        num_boxes = len(boxes)
+        unlocked_boxes = set()
+        unlocked_boxes.add(0)  # First box is unlocked by default
+
+        # Start with the keys in the first box
+        keys = boxes[0]
+
+        while keys:
+            new_keys = []
+
+            for key in keys:
+                if key < num_boxes and key not in unlocked_boxes:
+                    unlocked_boxes.add(key)
+                    new_keys.extend(boxes[key])
+
+            keys = new_keys
+
+        return len(unlocked_boxes) == num_boxes
