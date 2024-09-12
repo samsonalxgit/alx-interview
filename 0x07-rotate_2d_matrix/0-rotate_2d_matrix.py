4b8c9cd993778f530cc266e5e439815f0eaed5d7
Samson <samsonmanie2022@gmail.com>
2024-09-12 12:21:45 -0700

    rotate_matrix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -1,29 +1,12 @@
 #!/usr/bin/python3
-"""2D matrix rotation module.
-"""
+"""given an n x n 2d matrix
+rotate it 90 degrees clockwise"""
 
 
 def rotate_2d_matrix(matrix):
-    """Rotates an m by n 2D matrix in place.
     """
-    if type(matrix) != list:
-        return
-    if len(matrix) <= 0:
-        return
-    if not all(map(lambda x: type(x) == list, matrix)):
-        return
-    rows = len(matrix)
-    cols = len(matrix[0])
-    if not all(map(lambda x: len(x) == cols, matrix)):
-        return
-    c, r = 0, rows - 1
-    for i in range(cols * rows):
-        if i % rows == 0:
-            matrix.append([])
-        if r == -1:
-            r = rows - 1
-            c += 1
-        matrix[-1].append(matrix[r][c])
-        if c == cols - 1 and r >= -1:
-            matrix.pop(r)
-        r -= 1
+    rotate the matrix clockwise (90 degrees)
+    """
+    ziped = zip(*reversed(matrix))
+    for column1, column2 in enumerate(ziped):
+        matrix[column1] = list(column2)
